9d54823ac9ad08fb66bff8d016d055760975068a
David <davidfcasass22@gmail.com>
2025-11-16 10:39:11 -0500

    Imprimir recorrido:

--- RedTrofica/src/GrafoListaAdyacencia.java
+++ RedTrofica/src/GrafoListaAdyacencia.java
@@ -49,22 +49,25 @@ public class GrafoListaAdyacencia {
    
     public void BellamFord(int origenId, int destinoId){
         double[]dist = new double[vertices];
+        int[] predecesor = new int[vertices];
        
        //Inicializar en el arreglo el la distancia del NodoOrigen en 0 y las demas en "infinito"
        
         for(int i=0; i<vertices; i++){
             dist[i]=Double.MAX_VALUE;
+            predecesor[i] = -1;
         }
         dist[origenId] = 0;
        
-        for(int j=0; j<vertices-1; j++){
-            for(int k =0; k<vertices; k++){
-                for(Arista arista : adj[k]){
+        for(int j=0; j<vertices-1; j++){//for hasta el numero de vertices
+            for(int k =0; k<vertices; k++){//for que recorre todos nodos del grafo
+                for(Arista arista : adj[k]){//itera todas las aristas salientes del nodo k
                     int v = arista.destino.id;
                     double  p = arista.peso;
                     int o = arista.origen.id;
                     if(((dist[o]+p)<dist[v]) && (dist[o] != Double.MAX_VALUE)){
                         dist[v] = dist[o]+p;
+                        predecesor[v] = k;
                     }
                 }
             }
@@ -82,8 +85,14 @@ public class GrafoListaAdyacencia {
                     }
             }
         }
-       
-        System.out.println("El la energía total del camino mas eficiente es: " + (-1*dist[destinoId]));
+        if(dist[destinoId] == Double.MAX_VALUE){
+            System.out.println("No hay camino desde " + animales[origenId].name + " hasta " + animales[destinoId].name);
+            return;
+        }
+        else{
+            System.out.println("El la energía total del camino mas eficiente es: " + (-1*dist[destinoId]));
+            imprimirCamino(predecesor, origenId, destinoId);
+        }
     }
    
     public void imprimirGrafo() {
@@ -96,6 +105,19 @@ public class GrafoListaAdyacencia {
                 System.out.print("(Depredador: " + arista.destino.name + ", Peso: " + (-1*arista.peso) + ") ");
             }
             System.out.println();
+        }
+    }
+
+    private void imprimirCamino(int[] predecesor, int origenId, int destinoId){
+        if(destinoId == origenId){
+            System.out.print(animales[origenId].name + " ");
+        }
+        else if(predecesor[destinoId] == -1){
+            System.out.println("No hay camino desde " + animales[origenId].name + " hasta " + animales[destinoId].name);
+        }
+        else{
+            imprimirCamino(predecesor, origenId, predecesor[destinoId]);
+            System.out.print("-> " + animales[destinoId].name + " ");
         }
     }
 }
